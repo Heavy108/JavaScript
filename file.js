95ba06e559f89d2ce47d0648b94b53d9dc02b1c7
Heavy108 <Heavy1089@gmail.com>
2024-02-28 21:20:41 +0530

    28-02-2024

---
+++ file.js
@@ -0,0 +1,12 @@
+// Declare two variables: admin and name.
+// Assign the value "John" to name.
+// Copy the value from name to admin.
+// Show the value of admin using alert (must output “John”).
+
+let admin;
+let name = "john";
+
+admin = name;
+alert(NaN ** 0); // Outputs "
+console.log(9007199254740991 + 1); // 9007199254740992
+console.log(9007199254740991 + 2); // 9007199254740992
